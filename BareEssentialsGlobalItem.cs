75e4d217abe38f3b80014bd07e9e054ecbd34fc3
RasEza <rasmus.appel@gmail.com>
2020-03-24 19:34:12 +0100

    add missing functionality to mirror of remorse, bump version number

---
+++ BareEssentialsGlobalItem.cs
@@ -0,0 +1,18 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+
+namespace BareEssentials
+{
+    class BareEssentialsGlobalItem : GlobalItem
+    {
+        public override bool UseItem(Item item, Player player)
+        {
+            if (item.type == ItemID.MagicMirror || item.type == ItemID.IceMirror || item.type == ItemID.CellPhone)
+            {
+                player.GetModPlayer<BareEssentialsPlayer>().RemorsePosition = player.position;
+            }
+            return base.UseItem(item, player);
+        }
+    }
+}
